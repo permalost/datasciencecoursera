6309f25f0283e76cb758738e64b754cbd6d89d6c
PermaLost <permalost@gmail.com>
2014-08-17 10:57:26 -0600

    Working pollutantmean (except it shows more than 3 decimal places, unlike the examples)

--- RProgramming/Programming Assignment 1/pollutantmean.R
+++ RProgramming/Programming Assignment 1/pollutantmean.R
@@ -9,6 +9,18 @@ pollutantmean <- function(directory, pollutant, id = 1:332) {
   ## 'id' is an integer vector indicating the monitor ID numbers
   ## to be used
   
+  
+  data <- numeric()
+  
+  for (monitor in id) {
+    monitorFileName <- formatC(monitor, width=3, flag='0')
+    
+    file <- read.csv(paste(directory,"/",monitorFileName,".csv", sep = ""))
+    
+    data <- c(data, file[[pollutant]])    
+  }
+  
   ## Return the mean of the pollutant across all monitors list
   ## in the 'id' vector (ignoring NA values)
+  mean(data, na.rm=TRUE)
 }
--- RProgramming/Programming Assignment 1/tests/run_tests.R
+++ RProgramming/Programming Assignment 1/tests/run_tests.R
@@ -6,10 +6,19 @@ pollutant.test.suite <- defineTestSuite("pollutant.example",
                               dirs = file.path("tests"),
                               testFileRegexp = '^pollutantmean\\.R')
 
-complete.test.suite <- defineTestSuite("complete.example",
-                              dirs = file.path("tests"),
-                              testFileRegexp = '^complete\\.R')
 
-test.result <- runTestSuite(c(pollutant.test.suite,complete.test.suite))
+source('complete.R')
+
+complete.test.suite <- defineTestSuite("complete.example",
+                             dirs = file.path("tests"),
+                             testFileRegexp = '^complete\\.R')
+
+source('corr.R')
+
+corr.test.suite <- defineTestSuite("corr.example",
+                                       dirs = file.path("tests"),
+                                       testFileRegexp = '^corr\\.R')
+
+test.result <- c(runTestSuite(pollutant.test.suite),runTestSuite(complete.test.suite),runTestSuite(corr.test.suite))
 
 printTextProtocol(test.result)
