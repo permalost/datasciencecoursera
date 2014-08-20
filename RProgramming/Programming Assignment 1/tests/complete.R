76cefc055f0340ad18137ff8a35b98c189f83a80
PermaLost <permalost@gmail.com>
2014-08-19 20:21:36 -0600

    removing stuff

--- RProgramming/Programming Assignment 1/tests/complete.R
+++
@@ -1,7 +0,0 @@
-test.complete.examples <- function()
-{
-  checkEqualsNumeric(c(1,117), complete("specdata", 1))
-  checkEqualsNumeric(matrix(c(2,4,8,10,12,1041,474,192,148,96),ncol=2,nrow=5), complete("specdata", c(2, 4, 8, 10, 12)))
-  checkEqualsNumeric(matrix(c(30:25,932,711,475,338,586,463),ncol=2,nrow=6), complete("specdata", 30:25))
-  checkEqualsNumeric(c(3,243), complete("specdata", 3))
-}
--- RProgramming/Programming Assignment 1/tests/pollutantmean.R
+++
@@ -1,6 +0,0 @@
-test.pollutantmean.examples <- function()
-{
-  checkEqualsNumeric(1.281, pollutantmean("specdata", "nitrate", 23))
-  checkEqualsNumeric(4.064, pollutantmean("specdata", "sulfate", 1:10))
-  checkEqualsNumeric(1.706, pollutantmean("specdata", "nitrate", 70:72))
-}
